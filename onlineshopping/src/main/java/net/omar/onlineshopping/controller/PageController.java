8951bcf5e0f48f3928bde6d18360aefe6f9051c4
Omar Khalil <omar.tareefy@hotmail.com>
2017-08-01 20:25:50 +0300

    Compeleted the Interface theme and other issues

--- onlineshopping/src/main/java/net/omar/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/omar/onlineshopping/controller/PageController.java
@@ -2,7 +2,6 @@ package net.omar.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,20 +10,25 @@ public class PageController {
 	
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index(){
-		
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Spring Web MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
-	/*
-	@RequestMapping(value = {"/test"})
-	public ModelAndView test(@RequestParam(value = "anas", required = false) String anas){
-		if (anas == null){
-			anas = "im empty";
-		}
+	
+	@RequestMapping(value = {"/about"})
+	public ModelAndView about(){
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", anas);
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
 		return mv;
 	}
-	*/
+	
+	@RequestMapping(value = {"/contact"})
+	public ModelAndView contact(){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
 }
