cc58c52f389d340d050b12c5412854c6a30425c7
Omar Khalil <omar.tareefy@hotmail.com>
2017-08-04 21:53:39 +0300

    After creating the Product Entity

--- shoppingbackend/src/main/java/net/omar/shoppingbackend/daoimpl/CategoryDAOImpl.java
+++ shoppingbackend/src/main/java/net/omar/shoppingbackend/daoimpl/CategoryDAOImpl.java
@@ -3,59 +3,82 @@ package net.omar.shoppingbackend.daoimpl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import net.omar.shoppingbackend.dao.CategoryDAO;
 import net.omar.shoppingbackend.dto.Category;
 
+@Transactional
 @Repository("categoryDAO")
 public class CategoryDAOImpl implements CategoryDAO {
 
+	@Autowired
+	private SessionFactory sessionFactory;
+	
 	private static List<Category> categories = new ArrayList<>();
-	
-	static{
-		Category category = new Category();
-		
-		//Adding first Category
-		category.setId(1);
-		category.setName("Television");
-		category.setDescription("This is a description for Television");
-		category.setImageURL("CAT_1.png");
-		categories.add(category);
-		
-		//Adding second Category
-		category = new Category();
-		category.setId(2);
-		category.setName("Mobile");
-		category.setDescription("This is a description for Mobile");
-		category.setImageURL("CAT_2.png");
-		categories.add(category);
-		
-		//Adding third Category
-		category = new Category();
-		category.setId(3);
-		category.setName("Laptop");
-		category.setDescription("This is a description for Laptop");
-		category.setImageURL("CAT_3.png");
-		categories.add(category);
-	}
-	
 	
 	@Override
 	public List<Category> list() {
-		// TODO Auto-generated method stub
-		return categories;
+		
+		String selectActiveCategory = "FROM Category WHERE active = :active";
+		Query query = sessionFactory.getCurrentSession().createQuery(selectActiveCategory);
+		query.setParameter("active", true);
+		
+		return query.getResultList();
+	}
+
+	//Getting single category based on id
+	@Override
+	public Category get(int id) {
+		
+		return sessionFactory.getCurrentSession().get(Category.class, Integer.valueOf(id));
 	}
 
 
 	@Override
-	public Category get(int id) {
-		for (Category category : categories){
-			if (category.getId() == id){
-				return category;
-			}
+	public boolean add(Category category) {
+
+		try{
+			//Add the category to the database table
+			sessionFactory.getCurrentSession().persist(category);
+			return true;
+		}catch(Exception ex){
+			ex.printStackTrace();
+			return false;
 		}
-		return null;
+		
+	}
+
+
+	//Updating a single category
+	@Override
+	public boolean update(Category category) {
+		try{
+			//update the category
+			sessionFactory.getCurrentSession().update(category);
+			return true;
+		}catch(Exception ex){
+			ex.printStackTrace();
+			return false;
+		}
+	}
+
+
+	@Override
+	public boolean delete(Category category) {
+		category.setActive(false);
+		try{
+			//make the category not active (delete)
+			sessionFactory.getCurrentSession().update(category);
+			return true;
+		}catch(Exception ex){
+			ex.printStackTrace();
+			return false;
+		}
 	}
 
 }
--- shoppingbackend/src/main/java/net/omar/shoppingbackend/dto/Category.java
+++ shoppingbackend/src/main/java/net/omar/shoppingbackend/dto/Category.java
@@ -1,19 +1,35 @@
 package net.omar.shoppingbackend.dto;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Category {
 	
 	/*
 	 *Private fields 
 	 */
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String description;
+	@Column(name = "image_url")
 	private String imageURL;
+	@Column(name = "is_active")
 	private boolean active = true;
 	
 	
 	
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + ", description=" + description + ", imageURL=" + imageURL + ", active=" + active + "]";
+	}
+
 	public int getId() {
 		return id;
 	}
